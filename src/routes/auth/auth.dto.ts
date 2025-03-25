e9a57ddeb87d442d158cc2be0cae750b68357ec3
Huy Vo <forwork@minhuy.dev>
2025-03-25 14:46:54 +0700

    zod validation n serialization

--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,13 +1,29 @@
+import { UserStatus } from '@prisma/client'
 import { createZodDto } from 'nestjs-zod'
 import { z } from 'zod'
+
+const UserSchema = z.object({
+  id: z.number(),
+  email: z.string(),
+  name: z.string(),
+  phoneNumber: z.string(),
+  avatar: z.string().nullable(),
+  status: z.enum([UserStatus.ACTIVE, UserStatus.INACTIVE, UserStatus.BLOCKED]),
+  roleId: z.number(),
+  createdById: z.number().nullable(),
+  updatedById: z.number().nullable(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  deletedAt: z.date().nullable(),
+})
 
 const registerBodySchema = z
   .object({
     email: z.string().email(),
-    password: z.string().min(8),
-    confirmPassword: z.string().min(8),
-    name: z.string().min(3),
-    phoneNumber: z.string().optional(),
+    name: z.string().min(1).max(100),
+    password: z.string().min(8).max(100),
+    confirmPassword: z.string().min(8).max(100),
+    phoneNumber: z.string().min(9).max(15),
   })
   .strict()
   .superRefine(({ confirmPassword, password }, ctx) => {
@@ -21,3 +37,5 @@ const registerBodySchema = z
   })
 
 export class RegisterBodyDto extends createZodDto(registerBodySchema) {}
+
+export class RegisterResponseDto extends createZodDto(UserSchema) {}
